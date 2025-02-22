164f24f00284e4b87090a276e354c888b954c0dd
Bruno Noberto <brunorcnoberto@hotmail.com>
2025-02-22 22:38:30 +0000

    adicionado aula 7

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -137,4 +137,21 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       })
   });
 
+  it('Aula 7 Ex 1: Verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+    cy.contains('a', 'Política de Privacidade').should('have.attr', 'href', 'privacy.html').and('have.attr', 'target', '_blank')
+  });
+
+  it('Aula 7 Ex 2: Acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+    cy.contains('a', 'Política de Privacidade')
+      .invoke('removeAttr', 'target')
+        .click()
+    
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+  });
+
+  it.only('Aula 7 Ex 3: Testa a página da política de privacidade de forma independente', () => {
+    cy.visit('./src/privacy.html')
+    cy.contains('h1', 'CAC TAT - Política de Privacidade').should('be.visible')
+  });
+
 })
